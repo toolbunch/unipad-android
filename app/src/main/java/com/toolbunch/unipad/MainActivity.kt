ab451b37d0a9ce489875505970b1cfbec78258d8
Samyeer Metrani <toolbunch@gmail.com>
2022-07-10 16:31:59 -0700

    Initial commit

---
+++ app/src/main/java/com/toolbunch/unipad/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.toolbunch.unipad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
